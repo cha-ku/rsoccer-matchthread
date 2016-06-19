dba67361e836d652c84b505c71a10ec13545e6f0
Chaitanya Kukde <ChaKu@localhost.localdomain>
2016-06-20 02:34:01 +0530

    Changed a whole lot of things

--- get_soccer_update.py
+++ get_soccer_update.py
@@ -4,6 +4,7 @@
 import praw
 from urlparse import urlparse
 import time
+import re
 
 sleepytime = 30
 r = praw.Reddit(user_agent = 'cmdlinesoccer')
@@ -16,24 +17,27 @@ def UrlRoutine(thrd_url):
     return mtch_slftxt
 
 def getOpeningMin(mtch_thread_by_submission):
-    """ Searches for the opening minute ( 0' or 1') in the selftext.
-    If not found, asks for the starting word of the match events"""
-    if (mtch_thread_by_submission.selftext.lower().find("match events") != -1):
-        getKickOff_index = mtch_thread_by_submission.selftext.lower().find("match events")
-    if (mtch_thread_by_submission.selftext.lower().find("0'") != -1):
+    getKickOff_index = mtch_thread_by_submission.selftext.lower().find("match events")
+    if (getKickOff_index == -1):
         getKickOff_index = mtch_thread_by_submission.selftext.lower().find("0'")
-    elif (mtch_thread.selftext.lower().find("1'") != -1):
-        getKickOff_index = mtch_thread.selftext.lower().find("1'")
-    else:
-        getKickOff_index = mtch_thread.selftext.lower().find(raw_input("Input the starting word of the match events : "))
+        if (getKickOff_index == -1):
+            getKickOff_index = mtch_thread_by_submission.selftext.lower().find("1'")
+        else:
+            getKickOff_index = mtch_thread.selftext.lower().find(raw_input("Input the starting word of the match events : "))
     return getKickOff_index
 
+def CleanseAndPrint(match_thread , match_events_index):
+    """ Removes a bunch of unnecessary parts []#sprite6-p177 and []#icon-sub-big """
+    dirty_match_thread = match_thread.selftext[match_events_index : ]
+    clean_match_thread = re.sub(r'\[\]\([^)]*\)', '', dirty_match_thread)
+    return clean_match_thread
 
 try:
     while True:
         mtch_thread = UrlRoutine(getUrl)
         KickOff_index = getOpeningMin(mtch_thread)
-        print(mtch_thread.selftext[KickOff_index : ])
+        # print(mtch_thread.selftext[KickOff_index : ])
+        print CleanseAndPrint(mtch_thread , KickOff_index)
         time.sleep(sleepytime)
 except KeyboardInterrupt:
     print 'Back to work, eh?'

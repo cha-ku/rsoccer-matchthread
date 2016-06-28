babf5f5af548f7fac6d1622ffeb9aa313f8a19cf
chaiku <ckukde@gmail.com>
2016-06-28 21:26:53 +0530

    Changes made as suggested by /u/aurora0001

--- get_soccer_update.py
+++ get_soccer_update.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import praw
+
 from urlparse import urlparse
 import time
 import re
 
-def UrlRoutine(thrd_url , red):
+def url_routine(thrd_url , red):
     """ Returns the selftext  """
     football_url = urlparse(thrd_url)
     mtch_slftxt = red.get_submission(submission_id = football_url.path.split('/')[4])
     return mtch_slftxt
 
-def getOpeningMin(mtch_thread_by_submission, manual_entry):
+def get_opening_min(mtch_thread_by_submission, manual_entry):
     """Directly asks user for the match events keyword"""
     getKickOff_index = mtch_thread_by_submission.selftext.lower().find(manual_entry.lower())
     return getKickOff_index
 
-def CleanseAndPrint(match_thread , match_events_index):
+def clean_and_print(match_thread , match_events_index):
     """ Removes a bunch of unnecessary parts such as []#sprite6-p177 and []#icon-sub-big from the selftext """
     dirty_match_thread = match_thread.selftext[match_events_index : ]
     clean_match_thread = re.sub(r'\[\]\([^)]*\)', '', dirty_match_thread)
     return clean_match_thread
 
-def justPrintIt(mtch_thread, KO_indx):
-    print CleanseAndPrint(mtch_thread , KO_indx)
+def just_print(mtch_thread, KO_indx):
+    print clean_and_print(mtch_thread , KO_indx)
     time.sleep(30)
--- print2screen.py
+++ print2screen.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-getUrl = raw_input("Enter the match thread url: ")
-keyword = raw_input("Input the starting word(s) of the match events : ")
+import get_soccer_update as update
+import praw
+import time
+import os
 
 if __name__ == '__main__':
-    import get_soccer_update as update
-    import praw
-    import time
-    import os
+    usr_url = raw_input("Enter the match thread url: ")
+    keyword = raw_input("Input the starting word(s) of the match events : ")
 
     def dostuff(url):
         r = praw.Reddit(user_agent = 'cmdlinesoccer')
-        match_thread = update.UrlRoutine(url , r)
-        KickOff_index = update.getOpeningMin(match_thread, keyword)
-        update.justPrintIt(match_thread, KickOff_index)
+        match_thread = update.url_routine(url , r)
+        KickOff_index = update.get_opening_min(match_thread, keyword)
+        update.just_print(match_thread, KickOff_index)
 
     while True:
         try:
             os.system('cls' if os.name == 'nt' else 'clear')
-            dostuff(getUrl)
+            dostuff(usr_url)
             time.sleep(30)
         except KeyboardInterrupt:
             print "Back to work, eh?"
